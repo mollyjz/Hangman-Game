d2d278ad67b34e7cacf73e72669ecbbcf880d0f6
Molly Zurheide <you@example.com>
2018-04-01 13:23:51 -0400

    some more changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -54,9 +54,11 @@ var randomWords = [
 ];
 
 var selectedWord = ""; //word selected by computer
-var guess = event.key;
+var guess = KeyboardEvent.key;
 var lettersGuessed = [];
 var blank = [];
+var guessesLeft;
+var blanksLeft;
 
 
 //GLOBAL FUNCTIONS
@@ -73,28 +75,41 @@ function wasLetterEntered() {   //checks to make sure letter was entered (rather
     return true;
 }
 
-function wasLetterAlreadyUsed(guess) {   //checks to see if guess is in the lettersGuessed array and add it if not
-   if (lettersGuessed.indexOf(guess)) < 0) {
-        lettersGuessed.push(guess);
-    }
-} 
-
-function correctGuess() { //if guess is correct...
+function correctGuess(guess) { //if guess is correct...
     function replaceBlank() {
         for (var i = 0; i < selectedWord.length; i++) { //starting with first letter in word, until you get to the last letter, cycle through the letters one by one. display letter you're currently on.
-            console.log(selectedWord.charAt(i));
+            //console.log(selectedWord.charAt(i));
             //set blank array variable to the selected letter;
             blank[i] = selectedWord.charAt(i); //the blank corresponding to the letter you're on stores the letter you're at in the word
         }
     }
 }
 
-function wrongGuess() {
+function wrongGuess(guess) {
     function addToLettersGuessed() {
         document.getElementById("letters-guessed").innerHTML = guess; // print letter to "letters guessed"
         guessesLeft--; //subtract 1 from "guesses left"
     }
 }
+
+function wasLetterAlreadyUsed(guess) {   //checks to see if guess is in the lettersGuessed array and add it if not
+    if (lettersGuessed.indexOf(guess) < 0) {  //not sure why this is giving me an error
+         lettersGuessed.push(guess);
+     }
+ } 
+
+ function gameOver() {// conditions under which to end game
+	if (blanksLeft < 1) { //win
+		document.getElementById("game-over").innerHTML = "Great job--you won!! Let's play again"; // win
+        wins++;
+        document.getElementById("wins").innerHTML = wins;
+        restartGame();
+    } else if (guessesLeft < 1) {
+        document.getElementById("game-over").innerHTML = "Sorry, you lost. Let's try this again!"; // loss
+        restartGame();
+    }
+}
+
 
 function isGameOver() {     //check to see if round should end
     if (guessesLeft < 0 || blanksLeft < 1); {
@@ -120,17 +135,6 @@ function restartGame() {    //to restart game after win or loss
     // win count should still be there from last round
 }
 
-function gameOver() {    // conditions under which to end game
-	if (blanksLeft < 1) { //win
-		getElementById("game-over").innerHTML = "Great job--you won!! Let's play again"; // win
-        wins++;
-        getElementById("wins").innerHTML = wins;
-        restartGame(); }
-	} else { //loss
-        getElementById("game-over").innerHTML = "Sorry, you lost... Let's try this again!"; // loss
-        restartGame();
-    }
-}
 
 
 
@@ -145,9 +149,9 @@ document.onkeyup = function(event) { //once game has started, any time a key is 
     var guess = String.fromCharCode(event.keyCode).toLowerCase(); //uppercase letters converted to lowercase
         if (selectedWord.indexOf(guess) > -1) { //if letter guessed appears in selected word...
             correctGuess(); //replace the corresponding blank(s) with that letter
-            isGameOver(); //check to see if game is over
-        else { //if letter guessed is incorrect...
-            addToLettersGuessed(); //add that letter to "letters guessed" section of page
-            isGameOver(); //check to see if game is over
+            isGameOver(); //check to see if game is over and restart if so
+        }else { //if letter guessed is incorrect...
+            wrongGuess(guess); //add that letter to "letters guessed" section of page
+            isGameOver(); //check to see if game is over and restart if so
         }
     }
