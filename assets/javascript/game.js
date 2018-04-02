a4fb5bba76bf313fa63c8ad35f89d04fbb8c25a1
Molly Zurheide <you@example.com>
2018-04-01 20:23:02 -0400

    best i can do for now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -53,77 +53,58 @@ var randomWords = [
     "babboon"
 ];
 
-var selectedWord = ""; //word selected by computer
+var selectedWord = randomWords[Math.floor(Math.random() * randomWords.length)]; //word selected by computer
 var guess = KeyboardEvent.key;
 var lettersGuessed = [];
 var blank = [];
-var guessesLeft;
-var blanksLeft;
+var guessesLeft = 12;
+var blanksLeft = 7;
+var wins = 0;
 
 
 //GLOBAL FUNCTIONS
 
-function generateWord() { //generates random word
-    var selectedWord = randomWords[Math.floor(Math.random() * randomWords.length)];
+//generates random word
+function generateWord() { 
+    var selectedWord = randomWords[Math.floor(Math.random() * randomWords.length)]; 
 }
 
-function wasLetterEntered() {   //checks to make sure letter was entered (rather than number or other character)
+//checks to make sure letter was entered (rather than number or other character) and that it hasn't already been used this round
+function mainGame () { 
     if (window.event.keyCode < 66 || window.event.keyCode > 90) {
-        return false;
-    }
-
-    return true;
-}
-
-function correctGuess(guess) { //if guess is correct...
-    function replaceBlank() {
+        return false; //if not a letter, don't print
+    } else if (lettersGuessed.indexOf(guess) < 0) { //otherwise, if letter, if that letter not guessed before this round, push to "letters guessed" array
+        lettersGuessed.push(guess);
+    } if (selectedWord.indexOf(guess) > -1)  { //if guess is correct...
         for (var i = 0; i < selectedWord.length; i++) { //starting with first letter in word, until you get to the last letter, cycle through the letters one by one. display letter you're currently on.
-            //console.log(selectedWord.charAt(i));
-            //set blank array variable to the selected letter;
-            blank[i] = selectedWord.charAt(i); //the blank corresponding to the letter you're on stores the letter you're at in the word
-        }
-    }
-}
-
-function wrongGuess(guess) {
-    function addToLettersGuessed() {
+            blank[i] = selectedWord.charAt(i); //set blank array variable to the selected letter; the blank corresponding to the letter you're on stores the letter you're at
+    } else { //if guess is incorrect...
         document.getElementById("letters-guessed").innerHTML = guess; // print letter to "letters guessed"
         guessesLeft--; //subtract 1 from "guesses left"
     }
 }
 
-function wasLetterAlreadyUsed(guess) {   //checks to see if guess is in the lettersGuessed array and add it if not
-    if (lettersGuessed.indexOf(guess) < 0) {  //not sure why this is giving me an error
-         lettersGuessed.push(guess);
-     }
- } 
 
- function gameOver() {// conditions under which to end game
-	if (blanksLeft < 1) { //win
-		document.getElementById("game-over").innerHTML = "Great job--you won!! Let's play again"; // win
+function isGameOver() {     //check to see if round should end
+    if (blanksLeft < 1) {
+        document.getElementById("game-over").innerHTML = "Great job--you won!! Let's play again"; // win
         wins++;
         document.getElementById("wins").innerHTML = wins;
         restartGame();
-    } else if (guessesLeft < 1) {
+    } else if (guessesLeft < 0) {
         document.getElementById("game-over").innerHTML = "Sorry, you lost. Let's try this again!"; // loss
         restartGame();
     }
 }
 
-
-function isGameOver() {     //check to see if round should end
-    if (guessesLeft < 0 || blanksLeft < 1); {
-        gameOver();
-    }
-}
-
-function initializeGame() { //to start game for the first time
-    for (i=0; i++;) {
-    guessesLeft = 12;
-    blanksLeft = 7;
-    lettersGuessed = [];
-    wins = 0;
-    generateWord();
+//to start game for the first time when any key is pressed
+function initializeGame() {  
+    for (i = 0; i < 1; i++) {
+        guessesLeft = 12;
+        blanksLeft = 7;
+        lettersGuessed = [];
+        wins = 0;
+        generateWord();
     }
 }
 
@@ -141,17 +122,11 @@ function restartGame() {    //to restart game after win or loss
 
 // CALLING FUNCTIONS
 
-document.onkeyup = initializeGame(); //start game when user presses any key
+document.onload = initializeGame;//start game when user presses any key
 
-document.onkeyup = function(event) { //once game has started, any time a key is pressed...
-    wasLetterEntered(); //make sure the key pressed was a letter
-    wasLetterAlreadyUsed(); //make sure letter hasn't already been used this round
-    var guess = String.fromCharCode(event.keyCode).toLowerCase(); //uppercase letters converted to lowercase
-        if (selectedWord.indexOf(guess) > -1) { //if letter guessed appears in selected word...
-            correctGuess(); //replace the corresponding blank(s) with that letter
-            isGameOver(); //check to see if game is over and restart if so
-        }else { //if letter guessed is incorrect...
-            wrongGuess(guess); //add that letter to "letters guessed" section of page
-            isGameOver(); //check to see if game is over and restart if so
-        }
+document.onkeyup = function(event) { //once game has started, any time a key is pressed... 
+   console.log(event.key)
+    mainGame; //main game function
+   // if (selectedWord.indexOf(guess) > -1) { //if letter guessed appears in selected word... ////starts over
+        isGameOver; //check to see if game is over and restart if so
     }
