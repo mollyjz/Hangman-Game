0a4ae52d7df5dcbc8571afa8e5437e0cf97a13c8
Molly Zurheide <you@example.com>
2018-12-16 15:03:41 -0500

    fixed bugs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,3 @@
-//GLOBAL VARIABLES
-
 var randomWords = [
     "giraffe",
     "cheetah",
@@ -53,77 +51,77 @@ var randomWords = [
     "babboon"
 ];
 
-var selectedWord = randomWords[Math.floor(Math.random() * randomWords.length)]; //word selected by computer
-var guess = KeyboardEvent.key;
+var selectedWord = randomWords[Math.floor(Math.random() * randomWords.length)];
 var lettersGuessed = [];
-var blank = [];
+var correctGuesses = [];
 var guessesLeft = 12;
 var blanksLeft = 7;
 var wins = 0;
+var selectedWord = "";
 
-
-//GLOBAL FUNCTIONS
-
-//generates random word
-function generateWord() { 
-    var selectedWord = randomWords[Math.floor(Math.random() * randomWords.length)]; 
-}
-
-//checks to make sure letter was entered (rather than number or other character) and that it hasn't already been used this round
-function mainGame () { 
-    if (window.event.keyCode < 66 || window.event.keyCode > 90) {
+function mainGame(event) { 
+    var guess = event.key;
+    console.log("current letter guess: " + guess);
+    if (event.keyCode < 65 || event.keyCode > 90) {
         return false; //if not a letter, don't print
-    } else if (lettersGuessed.indexOf(guess) < 0) { //otherwise, if letter, if that letter not guessed before this round, push to "letters guessed" array
-        lettersGuessed.push(guess);
-    } if (selectedWord.indexOf(guess) > -1) { //if guess is correct...
-        for (var i = 0; i < selectedWord.length; i++) { //starting with first letter in word, until you get to the last letter, cycle through the letters one by one. display letter you're currently on.
-            blank[i] = selectedWord.charAt(i);
-     } //set blank array variable to the selected letter; the blank corresponding to the letter you're on stores the letter you're at
-    } else if (selectedWord.indexOf(guess) = -1) { //if guess is incorrect...
-        document.getElementById("letters-guessed").innerHTML = guess; // print letter to "letters guessed"
-        guessesLeft--; //subtract 1 from "guesses left"
-    } if (blanksLeft < 1) {
-        document.getElementById("game-over").innerHTML = "Great job--you won!! Let's play again"; // win
-        wins++;
-        document.getElementById("wins").innerHTML = wins;
-        restartGame();
-    } else if (guessesLeft < 0) {
-        document.getElementById("game-over").innerHTML = "Sorry, you lost. Let's try this again!"; // loss
-        restartGame();
-    }
-}
+    } else if ((lettersGuessed.indexOf(guess) < 0) && (lettersGuessed.length < 12) && (correctGuesses.indexOf(guess) < 0)) { //if letter not yet guessed this round...
+        
+        if (selectedWord.indexOf(guess) > -1) { //if guess is correct...
+            for (var i = 0; i < selectedWord.length; i++) {
+                if (selectedWord[i] == guess) {
+                    document.getElementById("blank" + i).innerHTML = selectedWord[i]; //fill in the blank
+                    blanksLeft--;
+                    correctGuesses.push(guess);
+                }
+            }
+        } else if (selectedWord.indexOf(guess) === -1) { //if guess is incorrect...
+            lettersGuessed.push(guess);
+            document.getElementById("letters-guessed").innerHTML = lettersGuessed.join(', '); //print to screen
+            guessesLeft--;
+            document.getElementById("guesses-left").innerHTML = guessesLeft;
+        };
+    
+        if (blanksLeft < 1) {
+            document.getElementById("game-over").innerHTML = "Great job--you won!! Let's play again!";
+            wins++;
+            document.getElementById("wins").innerHTML = wins;
+            restartGame();
+        } else if (guessesLeft < 1) {
+            guessesLeft = 0;
+            document.getElementById("guesses-left").innerHTML = guessesLeft;
+            document.getElementById("game-over").innerHTML = "Sorry, you lost. Let's play again!";
+            restartGame();
+        }    
+    };
+};
 
-
-
-//to start game for the first time when any key is pressed
-function initializeGame() {  
-    for (i = 0; i < 1; i++) {
-        guessesLeft = 12;
-        blanksLeft = 7;
-        lettersGuessed = [];
-        wins = 0;
-        generateWord();
-    }
-}
-
-function restartGame() {    //to restart game after win or loss
+function initializeGame() {
     guessesLeft = 12;
-    console.log (lettersGuessed);
     blanksLeft = 7;
     lettersGuessed = [];
-    generateWord();
-    // win count should still be there from last round
+    wins = 0;
+    selectedWord = randomWords[Math.floor(Math.random() * randomWords.length)];
+};
+
+function restartGame() { //to restart game after win or loss
+    document.getElementById("wins").innerHTML = wins;
+    blanksLeft = 7;
+    lettersGuessed = [];
+    correctGuesses = [];
+    function clear() {
+        document.getElementById("game-over").innerHTML = "";
+        document.getElementById("blanks").innerHTML = '<span id="blank0">_&nbsp;</span>' + '<span id="blank1">_&nbsp;</span>' + '<span id="blank2">_&nbsp;</span>' + '<span id="blank3">_&nbsp;</span>' + '<span id="blank4">_&nbsp;</span>' + '<span id="blank5">_&nbsp;</span>' + '<span id="blank6">_&nbsp;</span>';
+        document.getElementById("letters-guessed").innerHTML = "";
+        guessesLeft = 12;
+        document.getElementById("guesses-left").innerHTML = guessesLeft;
+        selectedWord = randomWords[Math.floor(Math.random() * randomWords.length)];
+    };
+    setTimeout(clear, 2000);
+};
+
+//////////////////////////////////////////////////////////
+
+window.onload = initializeGame;
+document.onkeyup = function(event) { 
+   mainGame(event);
 }
-
-
-
-
-
-// CALLING FUNCTIONS
-
-document.onload = initializeGame;//start game when user presses any key
-document.onkeyup = function(event) { //once game has started, any time a key is pressed... 
-   mainGame;
-   console.log(lettersGuessed)
- } //main game function 
-    // if (selectedWord.indexOf(guess) > -1) { //if letter guessed appears in selected word... ////starts over    }
